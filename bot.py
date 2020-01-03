81a68056f61d6c76e71cc324d8c37a9355ec979c
Pavel Buchinchik <p.buchinchik@gmail.com>
2020-01-03 04:18:28 +0300

    simple handlers

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ BOT_TOKEN = '1009278594:AAHsRiNo9n0gFWugQH2fcJog7mKHoF98iMQ'
 
 def hello(update, context):
     update.message.reply_text(
-        'Hello {}, args: {}'.format(update.message.from_user.first_name, context.args))
+        'Hello {}!'.format(update.message.from_user.first_name))
 
 def check_model(update, context):
     model_id = context.args[0]
@@ -14,7 +14,8 @@ def check_model(update, context):
         'Amount for {}: {}'.format(model_id, get_amount(model_id)))
 
 updater = Updater(BOT_TOKEN, use_context=True)
-updater.dispatcher.add_handler(CommandHandler('check', hello))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('check', check_model))
 
 updater.start_polling()
 updater.idle()
