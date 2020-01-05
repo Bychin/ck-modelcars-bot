f9e0208e7006cf984608db5de199f6d644b5d770
Pavel Buchinchik <p.buchinchik@gmail.com>
2020-01-05 20:29:23 +0000

    listing updates with photos

--- bot.py
+++ bot.py
@@ -1,31 +1,48 @@
+from telegram.error import TimedOut
 from telegram.ext import Updater, CommandHandler
 
 from parsers.f1_page import get_models_id
-from parsers.model import get_amount
+from parsers.model import Model
 from utils.cfg import get_param
 
 
 BOT_TOKEN = get_param('bot_token')
-CHAT_ID = '@ck_modelcars_scraper_bot'
+FRESH_LISTING_JOB = 'fresh_listings_job'
 
 
 def check_model(update, context):
     model_id = context.args[0]
     update.message.reply_text(
-        'Amount for {}: {}'.format(model_id, get_amount(model_id)))
+        'Amount for {}: {}'.format(model_id, Model(model_id).available_amount()))
 
 def update_fresh_listings(context):
+    chat_id = context.job.context
     models_id = get_models_id()
-    context.bot.send_message(
-        chat_id=context.job.context, text='\n'.join(models_id))
+
+    for model_id in models_id:
+        m = Model(model_id, parse=True)
+        try:
+            context.bot.send_photo(chat_id, m.img_urls[0], caption=m.full_name, timeout=5)
+        except TimedOut:
+            context.bot.send_message(chat_id, m.full_name)
 
 def subscribe_for_fresh_listings(update, context):
-    context.job_queue.run_repeating(update_fresh_listings, interval=60, first=5, context=update.message.chat_id)
+    job = context.job_queue.run_repeating(
+        update_fresh_listings, interval=60, first=5, context=update.message.chat_id)
+    context.chat_data[FRESH_LISTING_JOB] = job
+
     update.message.reply_text(
         'You have successfully subscribed for receiving notifications about new listings!')
 
 def unsubscribe_from_fresh_listings(update, context):
-    context.job_queue.stop()
+    if FRESH_LISTING_JOB not in context.chat_data:
+        update.message.reply_text('You do not have an active subscription for new listings.')
+        return
+
+    job = context.chat_data[FRESH_LISTING_JOB]
+    job.schedule_removal()
+    del context.chat_data[FRESH_LISTING_JOB]
+
     update.message.reply_text(
         'You have successfully unsubscribed from receiving notifications about new listings.')
 
@@ -39,4 +56,3 @@ updater.dispatcher.add_handler(
 
 updater.start_polling()
 updater.idle()
-
--- parsers/model.py
+++ parsers/model.py
@@ -1,16 +1,54 @@
+from string import Template
+
 from bs4 import BeautifulSoup
 import requests
 
-from .constants import CK_ADD_TO_CART_API, MODELS_LIMIT
+from .constants import CK_URL, CK_ADD_TO_CART_API, MODELS_LIMIT
 
 
-def get_amount(model_id):
-    with requests.get(CK_ADD_TO_CART_API, params={'produkt': model_id, 'menge': MODELS_LIMIT}) as r:
-        soup = BeautifulSoup(r.text, 'lxml')
+URL_TEMPLATE = Template(CK_URL+'p-$id/')
 
-    el = soup.find('input', {'name': 'menge_'+str(model_id)})
-    if el is None:
-        return 0
 
-    return el.get('value')
+class Model():
+    def __init__(self, model_id, parse=False):
+        self.id = model_id
+        self.url = self.__url()
 
+        if parse:
+            self.parse_info()
+
+    def parse_info(self):
+        with requests.get(self.url) as r:
+            soup = BeautifulSoup(r.text, 'lxml')
+
+        full_name = None
+        img_urls = []
+
+        tags_with_img = soup.findAll(class_='div_shopcontent_leftimage')
+        for tag in tags_with_img:
+            a_tag = tag.find('a')
+            img_urls.append(a_tag.get('href'))
+
+            if full_name is None:
+                full_name = a_tag.get('title')
+
+        self.full_name = full_name
+        self.img_urls = img_urls
+
+    def available_amount(self):
+        with requests.get(CK_ADD_TO_CART_API, params={'produkt': self.id, 'menge': MODELS_LIMIT}) as r:
+            soup = BeautifulSoup(r.text, 'lxml')
+
+        amount_in_cart = soup.find('input', {'name': 'menge_'+str(self.id)})
+        if amount_in_cart is None:
+            return 0
+
+        return amount_in_cart.get('value')
+
+    '''
+    Returns URL of model by it's id.
+
+    E.g. for id 12345 returns 'https://ck-modelcars.de/en/p-12345/'
+    '''
+    def __url(self):
+        return URL_TEMPLATE.substitute(id=self.id)
